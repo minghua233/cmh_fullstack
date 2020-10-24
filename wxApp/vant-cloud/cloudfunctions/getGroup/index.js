a07b6babdbebd7c08339e5e9649e24a1a46db87a
minghua233 <1540684370@qq.com>
2020-10-24 18:53:09 +0800

    vant-cloud

--- wxApp/vant-cloud/cloudfunctions/getGroup/index.js
+++ wxApp/vant-cloud/cloudfunctions/getGroup/index.js
@@ -5,5 +5,10 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  // const openId = event.userInfo.openId
+  const openId = cloud.getWXContext().OPENID
+  // 查找user-group这个库里是否有这个openId // 查找where() .get()
+  // 按照这个openId把user-group里数据取出
+  // 去group这个库里面查找所有_id和groupId相同的数据
   
 }
