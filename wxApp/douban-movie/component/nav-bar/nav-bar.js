06d9d1e0591e24ee33c897b8ff7b14da36492435
minghua233 <1540684370@qq.com>
2020-10-27 15:31:13 +0800

    douban-movie

---
+++ wxApp/douban-movie/component/nav-bar/nav-bar.js
@@ -0,0 +1,25 @@
+// component/nav-bar/nav-bar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    topHeight: 0,
+    statusBarStyle: '',
+    navBarStyle: ''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
