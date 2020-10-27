5dba4e9da2cfaeb72b176a960404bfdc193c978b
minghua233 <1540684370@qq.com>
2020-10-27 16:58:06 +0800

    douban-movie

--- wxApp/douban-movie/component/nav-bar/nav-bar.js
+++ wxApp/douban-movie/component/nav-bar/nav-bar.js
@@ -4,7 +4,22 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    title: {
+      type: String,
+      value: ''
+    },
+    statusBarColor: {
+      type: String,
+      value: '#fff'
+    },
+    navBarColor: {
+      type: String,
+      value: '#fff'
+    },
+    titleColor: {
+      type: String,
+      value: '#000'
+    }
   },
 
   /**
@@ -14,6 +29,23 @@ Component({
     topHeight: 0,
     statusBarStyle: '',
     navBarStyle: ''
+  },
+
+  lifetimes: {
+    attached: function () {
+      // 在组件实例进入页面节点树时执行
+      let navBarStyle = `background-color: ${this.data.navBarColor}; height: ${wx.db.navBarHeight}px; color: ${this.data.titleColor}`
+      let statusBarStyle = `background-color: ${this.data.statusBarColor}; height: ${wx.db.statusBarHeight}px`
+      let topHeight = wx.db.navBarHeight + wx.db.statusBarHeight
+      this.setData({
+        navBarStyle,
+        statusBarStyle,
+        topHeight
+      })
+    },
+    detached: function () {
+      // 在组件实例被从页面节点树移除时执行
+    },
   },
 
   /**
