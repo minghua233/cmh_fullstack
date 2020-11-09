7e87bcd57ea1e383f323ec3e93f4d2b5652f6ebf
minghua233 <1540684370@qq.com>
2020-11-09 16:04:52 +0800

    go 爬虫

--- go/go_demo/main.go
+++ go/go_demo/main.go
@@ -2,24 +2,35 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func BaiduHotSearch() {
 	// http 把网页请求
 	// dom 内存第三方分析
 	// find
-	res, err := http.Get("http://www.baid.com/yedashi")
+	res, err := http.Get("http://www.baid.com")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s",res.StatusCode, res.Status)
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(".s-news-rank-content .news-meta-item").Each(func(i int, s *goquery.Selection) {
+		content := s.Find(".title-content").Text()
+		fmt.Printf("%d: %s\n", i, content)
+	})
 }
 
 func main() {
-	BaiduHotSearch() 	//模块化
+	BaiduHotSearch() //模块化
 }
