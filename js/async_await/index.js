066cd23b9452aae3dc44c940972f73bcde44c789
minghua233 <1540684370@qq.com>
2020-11-12 15:10:22 +0800

    async_await

--- js/async_await/index.js
+++ js/async_await/index.js
@@ -1,14 +1,33 @@
-function a() {
-  return new Promise ((resolve, reject) => {
+// function a() {
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('aaaaa');
+//       resolve('ok')
+//     }, 1000)
+//   })
+// }
+
+// function b() {
+//   console.log('bbbbb');
+// }
+
+// a().then(b)
+
+
+function timeout (ms) {
+  return new Promise(resolve => {
     setTimeout(() => {
-      console.log('aaaaa');
-      resolve('ok')
-    }, 1000)
+      console.log('你好');
+      resolve('')
+    }, ms)
   })
 }
 
-function b() {
-  console.log('bbbbb');
+// async声明函数内部可能存在异步的情况
+async function asyncPrint(value, ms) {
+  await timeout(ms) // 阻塞同步函数
+  console.log(value); // 去到微任务队列
 }
 
-a().then(b)
+asyncPrint('hello world', 1000)
+console.log('ok');
