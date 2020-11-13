6ac6f9b139df28f755c86f9308a5270460b96e0a
minghua233 <1540684370@qq.com>
2020-11-13 15:43:16 +0800

    node 引入

---
+++ node/node-learn/commonJS/index.js
@@ -0,0 +1,5 @@
+// console.log('start require');
+// console.log(require('./lib.js'));
+// console.log('end require');
+
+let lib = require('./lib.js')
---
+++ node/node-learn/commonJS/lib.js
@@ -0,0 +1,14 @@
+// console.log('hello world');
+
+// exports.hello = 'world'
+// exports.add = function (a, b) {
+//   return a + b
+// }
+
+setTimeout(() => {
+  console.log(exports);
+}, 1000)
+
+// module.exports = function minus(a, b) {
+//   return a - b
+// }
