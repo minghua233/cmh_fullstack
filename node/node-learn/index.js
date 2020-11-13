12f527bfd538ff1ecd03558068ca42fe28f71efb
minghua233 <1540684370@qq.com>
2020-11-13 15:42:58 +0800

    node 学习

---
+++ node/node-learn/index.js
@@ -0,0 +1,7 @@
+// console.log(requestAnimationFrame); // 浏览器运行的下一帧，因为不是浏览器环境，所以不存在
+// console.log(setImmediate);
+
+// console.log(__filename);  // 当前所运行的脚本所在的位置
+// console.log(__dirname);  // 当前所运行的脚本所在的目录位置
+
+console.log(process);
---
+++ node/node-learn/readme.md
@@ -0,0 +1,6 @@
+# node.js 是一个基于chrome v8 引擎的一个js运行环境
+# node.js 使用一个事件驱动，非阻塞式的IO模型，轻量且高效
+
+
+
+# commonJS ECMAScript
