ae477fe345cd51b0008ca8bc4f0146d08b5fad66
minghua233 <1540684370@qq.com>
2020-11-18 16:32:33 +0800

    微信小程序 小说

--- wxApp/book/miniprogram/pages/bookSection/bookSection.js
+++ wxApp/book/miniprogram/pages/bookSection/bookSection.js
@@ -11,7 +11,9 @@ Page({
     pageArray: [],
     page: 1, // 当前页
     pre: '',
-    next: ''
+    next: '',
+    preAble: false,
+    nextAble: false
   },
 
   getSection(url) {
@@ -26,31 +28,71 @@ Page({
     }).then(res => {
       // console.log(res);
       wx.hideLoading()
-      const {result} = res
+      const {
+        result
+      } = res
+      // console.log(result.next.split('/'));
       this.setData({
         bookDetailData: result.bookDetailData,
         lastData: result.lastData,
         pageData: result.pageData,
         pageArray: result.pageArray,
         pre: result.pre,
-        next: result.next
+        next: result.next,
+        preAble: result.pre === '' ? true : false,
+        nextAble: result.next === '' ? true : false,
+        page: Number(result.next.split('/')[2]) - 1
       })
     })
   },
 
   // 上一页
   prePage() {
+    if (this.data.preAble) return
     this.getSection(this.data.pre)
   },
+
+  // 下一页
   nextPage() {
+    if (this.data.nextAble) return
     this.getSection(this.data.next)
+  },
+
+  // 选择页面
+  changePage(e) {
+    console.log(e);
+    let page = Number(e.detail.value)
+    if (page !== this.page) {
+      this.setData({
+        page: page + 1
+      })
+      this.getSection(this.data.pageArray[page].name)
+    }
+  },
+
+  // 去看小说
+  navtoUrl(e) {
+    console.log(e);
+    let url = e.currentTarget.dataset.url
+    // 已经存在书架的书，记录阅读状态
+
+    wx.navigateTo({
+      url: `../bookContent/bookContent?url=${url}&name=${this.data.bookDetailData.name}&imgUrl=${this.data.bookDetailData.imgurl}`
+    })
+  },
+
+  // 加入书架 
+  joinBook(e) {
+    let url = e.currentTarget.dataset.url 
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     // console.log(options);
-    const {url} = options
+    const {
+      url
+    } = options
     this.getSection(url)
 
   },
