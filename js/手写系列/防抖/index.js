66623960441eec1ad9a8408cd2cd9ada44a2fbb4
minghua233 <1540684370@qq.com>
2020-11-19 16:55:42 +0800

    js 防抖

--- js/手写系列/防抖/index.js
+++ js/手写系列/防抖/index.js
@@ -1,4 +1,12 @@
 // 在一定时间内，如果持续请求，就一直不执行，在规定时间内没有第二次操作，才执行
 function debounce(fn, delay) {
+  let timer = null
 
+  return function () {
+    let arg = arguments
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn(arg)
+    }, delay);
+  }
 }
