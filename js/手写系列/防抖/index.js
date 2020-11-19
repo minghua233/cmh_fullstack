06115cb887154a320465edc2663c6964fdaf1255
minghua233 <1540684370@qq.com>
2020-11-19 17:00:47 +0800

    js 防抖

--- js/手写系列/防抖/index.js
+++ js/手写系列/防抖/index.js
@@ -1,12 +1,13 @@
 // 在一定时间内，如果持续请求，就一直不执行，在规定时间内没有第二次操作，才执行
 function debounce(fn, delay) {
-  let timer = null
+  let timer = null // 未被垃圾回收机制回收的变量，不会执行该赋值语句
 
-  return function () {
-    let arg = arguments
+  return function () { // 通过闭包保存上一次作用域
+    let arg = arguments,
+      context = this // 获得调用函数内部的this
     clearTimeout(timer)
     timer = setTimeout(() => {
-      fn(arg)
+      fn.apply(context, arg) // 保证this不被更改
     }, delay);
   }
 }
