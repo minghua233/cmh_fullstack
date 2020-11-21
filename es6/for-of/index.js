f7855f117833cf23613f0c9e1ce66abfebc88367
minghua233 <1540684370@qq.com>
2020-11-21 12:03:37 +0800

    for...in与for...of

--- es6/for-of/index.js
+++ es6/for-of/index.js
@@ -38,3 +38,22 @@ let obj = {
 //   console.log(arr[key]);
 // }
 
+function Foo() {
+  this[100] = 'test-100'
+  this[1] = 'test-1'
+  this['b'] = 'bar-B'
+  this[50] = 'test-50'
+  this[9] = 'test-9'
+  this[8] = 'test-8'
+  this[3] = 'test-3'
+  this[5] = 'test-5'
+  this['A'] = 'bar-A'
+  this['C'] = 'bar-C'
+}
+ let bar = new Foo()
+
+for (let key in bar) {
+  console.log(`index:${key} value:${bar[key]}`);
+}
+
+// ECMAScript规范规定,数字属性应该按照索引值的大小升序排列,字符串属性根据创建时的顺序排列
