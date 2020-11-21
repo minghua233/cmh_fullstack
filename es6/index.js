f9b977f6e5130a8b4a02ee7132370c2e3f93115d
minghua233 <1540684370@qq.com>
2020-11-21 12:04:52 +0800

    es6基础语法

---
+++ es6/index.js
@@ -0,0 +1,12 @@
+// var foo = 'bar
+// console.log(foo);
+// let foo = 'bar'
+// var 有变量提升，let没有
+
+// const foo = 'bar'
+// foo = 123 // 更改const 定义的常量值报错
+// console.log(foo);
+
+// 模板拼接使用``，变量用${}包裹
+let name = "Tom"
+let str = `My name is ${name}.`
