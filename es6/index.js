b3024cdcd69df8a71c9f461dcc296c930a285c42
minghua233 <1540684370@qq.com>
2020-11-22 18:59:19 +0800

    es6

--- es6/index.js
+++ es6/index.js
@@ -8,5 +8,50 @@
 // console.log(foo);
 
 // 模板拼接使用``，变量用${}包裹
-let name = "Tom"
-let str = `My name is ${name}.`
+// let name = "Tom"
+// let str = `My name is ${name}.`
+
+
+
+// es6 新增的数组方法
+let arr = ['a', 'b', 'c']
+// console.log(Object.keys(arr));
+// let newArr = [...arr.keys()] //新增，直接取得数组下标
+// console.log(newArr);
+
+// let iter = arr.entries()  // 取到数组的键值对
+// for(let e of iter) {
+//   console.log(e);
+// }
+
+// let eArr = arr.values() // 通过values()取得数组所有元素值
+// for (let e of eArr) {
+//   console.log(e);
+// }
+
+// function test(fruit) { // 通过includes判断数组内是否有该值
+//   const redFruits = ['apple', 'cherry']
+//   if( redFruits.includes(fruit)) {
+//     console.log('red');
+//   }
+// }
+
+
+// let people = [
+//   {name: "Tom", age: 20},
+//   {name: "Peter", age: 22},
+//   {name: "Jane", age: 18}
+// ]
+// function findFriend(person) {
+//   return person.name === 'Tom'
+// }
+// console.log(people.find(findFriend)); // 通过find查找满足指定条件是数组的值
+
+
+
+let test = ['a', 'b', 'c']  
+function index(i) {
+  return i === 'b'
+}
+console.log(test.findIndex(index)); // 通过find查找满足指定条件是数组的下标
+
