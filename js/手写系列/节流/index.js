b7638a970f6d966d22d648d4013c508e9bc71ebd
minghua233 <1540684370@qq.com>
2020-11-23 19:05:40 +0800

    js 节流

---
+++ js/手写系列/节流/index.js
@@ -0,0 +1,14 @@
+// 在规定的时间内只执行一次
+
+function throttle(fn, delay) {
+  let prev = Date.now() // 上一次点击
+  return function () {
+    let now = Date.now() // 这一次点击
+    let context = this
+    let arg = arguments
+    if (now - prev >= delay) {
+      fn.apply(context, arg)
+      prev = Date.now()
+    }
+  }
+}
