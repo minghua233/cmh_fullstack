b8baadeb53303f2a9e244eaf91114ddec576d4ba
minghua233 <1540684370@qq.com>
2020-11-26 16:26:37 +0800

    vue2.0

---
+++ vue/vue-element/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-link>首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {
+
+  }
+};
+</script>
+
+<style>
+</style>
+
