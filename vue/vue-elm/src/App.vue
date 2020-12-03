046db09a2fa1ed7d7f900d3bd11ef8e4753b93be
minghua233 <1540684370@qq.com>
2020-12-03 15:37:06 +0800

    vue饿了么

--- vue/vue-elm/src/App.vue
+++ vue/vue-elm/src/App.vue
@@ -5,14 +5,29 @@
 </template>
 
 <script>
-import Header from '@/components/header/Header'
+import Header from "@/components/header/Header";
+import { getSeller } from "@/api";
+import qs from 'query-string'
 export default {
+  data() {
+    return {
+      seller: {
+        id: qs.parse(location.search).id
+      }
+    }
+  },
   components: {
     Header
+  },
+  created() {
+    getSeller({
+      id: this.seller.id
+    }).then(seller => {
+      console.log(seller)
+    });
   }
-}
+};
 </script>
 
 <style>
-
 </style>
