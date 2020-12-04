7b417063c89391761558979dadb8109fa1e82602
minghua233 <1540684370@qq.com>
2020-12-04 16:09:29 +0800

    vue饿了么

--- vue/vue-elm/src/api/index.js
+++ vue/vue-elm/src/api/index.js
@@ -3,7 +3,7 @@ import {
 } from './helper'
 
 const getSeller = get('api/seller') // 获取商家信息的接口
-const getGoods = get('api/goods')
+const getGoods = get('api/goods') // 获取商品的接口
 
 export {
   getSeller,
