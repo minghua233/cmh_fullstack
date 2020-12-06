38d6fd416f7400e65374a35e9d3ffde3bc47d4c3
minghua233 <1540684370@qq.com>
2020-12-06 16:57:37 +0800

    vue饿了么

--- vue/vue-elm/src/components/header-detail/Header-detail.vue
+++ vue/vue-elm/src/components/header-detail/Header-detail.vue
@@ -19,7 +19,7 @@
               :key="index"
             >
               <support-ico
-                :size=1
+                :size=2
                 :type=item.type
               ></support-ico>
               <span class="text">{{item.description}}</span>
@@ -31,11 +31,11 @@
             <div class="line"></div>
           </div>
           <div class="bulletin">
-            <p class="content">粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0添加的良心品质深得消费者青睐，发展至今成为粥类的引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。</p>
+            <p class="content">{{seller.bulletin}}</p>
           </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" @click.stop="hide">
         <i class="icon-close"></i>
       </div>
     </div>
@@ -55,6 +55,11 @@ export default {
   },
   components: {
     SupportIco
+  },
+  methods: {
+    hide() {
+      this.$emit('hide', false)
+    }
   }
 };
 </script>
