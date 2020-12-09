62afa4e7c7cf6f16fecd8b59bbef3c900079c627
minghua233 <1540684370@qq.com>
2020-12-09 16:16:09 +0800

    vuex

---
+++ vue/vuex/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <count></count>
+  </div>
+</template>
+
+<script>
+import count from './components/count.vue'
+
+
+export default {
+  components: { count },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vuex/src/components/count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="count">
+    <h3>{{$store.state.count}}</h3>
+    <h2>{{count}}</h2>
+    <h1>{{newCount}}</h1>
+    <!-- <h1>{{msg}}</h1> -->
+    <p>
+      <button @click="addAction">add</button>
+      <button @click="$store.commit('reduce')">reduce</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex';
+
+export default {
+  // computed: {
+  //   newCount () {
+  //     return this.$store.state.count * 2
+  //   }v
+  // },
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  // computed: mapState({
+  //   newCount: state => state.count
+  // }),
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['newCount'])
+  },
+  methods: {
+    test() { },
+    ...mapMutations(['reduce']),
+    ...mapActions(['addAction'])
+  }
+
+  // mounted() {
+  //   this.msg = mapState(['count'])
+  // }
+  // computed: mapState(['count'])
+}
+</script>
+
+<style>
+</style>
