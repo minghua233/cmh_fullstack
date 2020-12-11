8aec0652ce787f6873c945bbad21238902002e0a
minghua233 <1540684370@qq.com>
2020-12-11 15:27:15 +0800

    vue全栈

--- vue/vue-mongodb/daike-client/axios/api.js
+++ vue/vue-mongodb/daike-client/axios/api.js
@@ -1,1 +1,13 @@
 import axios from 'axios'
+import config from './config'
+
+export default function $axios(options) { 
+  return new Promise((resolve, reject) => {
+    const instance = axios.create({
+      baseURL: config.baseURL,
+    })
+
+    // 请求拦截
+    
+  })
+}
