1ca349568636e817a0a33c31f77a512b2fa93950
minghua233 <1540684370@qq.com>
2020-12-13 17:36:04 +0800

    vue全栈

--- vue/vue-mongodb/daike-server/app.js
+++ vue/vue-mongodb/daike-server/app.js
@@ -2,6 +2,17 @@ const Koa = require('koa')
 const cors = require('koa2-cors')
 const bodyParser = require('koa-bodyparser')
 const user_router = require('./routes/api/user_router')
+const mongoose = require('mongoose') // 做mongodb的连接
+const config = require('./config')
+
+// 建立mongodb连接
+mongoose.connect(config.db, {useNewUrlParser: true}, (err) => {
+  if(err){
+    console.error('failed');
+  } else {
+    console.log('success');
+  }
+})
 
 const app = new Koa()
 
