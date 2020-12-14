8501200ecf6342e0842d9cb958021b8013832b81
minghua233 <1540684370@qq.com>
2020-12-14 15:57:09 +0800

    node mvc

--- node/koa-blog/index.js
+++ node/koa-blog/index.js
@@ -1,6 +1,6 @@
 const Koa = require('koa')
 const config = require('./config/default')
-
+const session = require('koa-session-minimal')
 const app = new Koa()
 // const main = (ctx) => {
 //   ctx.response.body = 'Hello world'
