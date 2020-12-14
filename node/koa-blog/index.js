26d70cc98b6babdba5adf5188f8118a7431d7f95
minghua233 <1540684370@qq.com>
2020-12-14 16:08:08 +0800

    node mvc

--- node/koa-blog/index.js
+++ node/koa-blog/index.js
@@ -1,10 +1,22 @@
 const Koa = require('koa')
+const path = require('path')
 const config = require('./config/default')
 const session = require('koa-session-minimal')
+const views = require('koa-views')
 const app = new Koa()
 // const main = (ctx) => {
 //   ctx.response.body = 'Hello world'
 // }
+
+// session 存储配置
+const sessionMysqlConfig = {
+
+}
+
+// 配置服务端的模板引擎中间件
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}))
 
 app.use(require('./routers/signin').routes())
 
