1ff4e89b574d3f530a70bd5dfb4792081f2fed52
minghua233 <1540684370@qq.com>
2020-12-16 15:48:22 +0800

    noteBook全栈

---
+++ vue/nootbook/nootbook-server/controllers/mySqlConfig.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql')
+const config = require('./defaultConfig')
+
+// 连接线程池
+let pool = mysql.createPool({
+  host: config.dataBase.HOST,
+  user: config.dataBase.USERNAME,
+  password: config.dataBase.PASSWORD,
+  database: config.dataBase.DATABASE,
+  port: config.dataBase.PORT
+})
+
+let allServices = {
+  query: function(sql, values) {
+    return new Promise((resolve, reject) => {
+      pool.getConnection(function(err, connection) {
+        if (err) {
+          reject(err)
+        } else {
+          connection.query(sql, values, (err, rows) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(rows)
+            }
+            connection.release()
+          })
+        }
+      })
+    })
+  }
+}
+
+// 用户登录
+let userLogin = function(username, userpwd) {
+  let _sql = `select * from users where username="${username}" and userpwd="${userpwd}";`
+  return allServices.query(_sql)
+}
+
+// 查找用户
+let findUser = function (username) {
+  let _sql = `select * from users where username="${username}";`
+  return allServices.query(_sql)
+}
+
+// 注册用户
+let insertUser = function(value) {
+  let _sql = `insert into users set username=?,userpwd=?,nickname=?;`
+  return allServices.query(_sql, value)
+}
+
+module.exports = {
+  userLogin,
+  findUser,
+  insertUser
+}
---
+++ vue/nootbook/nootbook-server/routes/users.js
@@ -0,0 +1,95 @@
+const router = require('koa-router')()
+const userService = require('../controllers/mySqlConfig')
+
+router.prefix('/users')
+
+// router.get('/', function (ctx, next) {
+//   ctx.body = 'this is a users response!'
+// })
+
+// router.get('/bar', function (ctx, next) {
+//   ctx.body = 'this is a users/bar response'
+// })
+
+// 注册
+router.post('/userRegister', async (ctx, next) => {
+  let _username = ctx.request.body.username;
+  let _userpwd = ctx.request.body.userpwd;
+  let _nickname = ctx.request.body.nickname
+  // console.log();
+  if (!_username || !_userpwd || !_nickname) {
+    ctx.body = {
+      code: '80001',
+      mess: '用户名密码和昵称不能为空'
+    }
+    return
+  }
+  await userService.findUser(_username).then(async (res) => {
+    console.log(res);
+    if (res.length) {
+      ctx.body = {
+        code: '80003',
+        mess: '用户名已存在'
+      }
+    } else {
+      await userService.insertUser([_username, _userpwd, _nickname]).then(res => {
+        console.log(res);
+        let r = ''
+        if (res.affectedRows != 0) {
+          r = 'ok'
+          ctx.body = {
+            code: '80000',
+            data: r,
+            mess: '注册成功'
+          }
+        } else {
+          r = 'error'
+          ctx.body = {
+            code: '80000',
+            data: r,
+            mess: '注册失败'
+          }
+        }
+      })
+    }
+  })
+})
+
+// 登录
+router.post('/userLogin', async (ctx, next) => {
+  let _username = ctx.request.body.username;
+  let _userpwd = ctx.request.body.userpwd;
+
+  // 把参数拿到数据库里查询
+  await userService.userLogin(_username, _userpwd).then(res => {
+    console.log(res);
+    let r = '';
+    if (res.length) {
+      r = 'ok';
+      let result = {
+        id: res[0].id,
+        nickname: res[0].nickname,
+        username: res[0].username
+      }
+      ctx.body = {
+        code: '80000',
+        data: result,
+        mess: '登录成功'
+      }
+    } else {
+      r = 'error'
+      ctx.body = {
+        code: '80004',
+        data: r,
+        mess: '账号或密码错误'
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: '80002',
+      data: err
+    }
+  })
+})
+
+module.exports = router
