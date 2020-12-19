2ceb00ef66070bf6dd1832a75d4a6f5eaf517600
minghua233 <1540684370@qq.com>
2020-12-19 12:00:05 +0800

    js 数组去重

--- js/手写系列/数组去重/filter.js
+++ js/手写系列/数组去重/filter.js
@@ -1,7 +1,14 @@
+// function unique(arr) {
+//   // return arr.filter((item, index) => arr.indexOf(item) === index); //时间复杂度n^2
+//   return arr.concat().sort().filter((item, index) => !index || item !== arr[index -1]); // 时间复杂度2n
+// }
+
 function unique(arr) {
-  // 如果新数组的当前元素的索引值 == 该元素在原始数组中的第一个索引，则返回当前元素
-  return arr.filter((item, index) => arr.indexOf(item, 0) === index);
+  // return Array.from(new Set(arr))
+  return [...new Set(arr)]
 }
+
+// let unique = (a) => [...new Set(a)]
 
 let arr = [1, 2, 3, 1, '1', '1', '3', 'a', 'A']
 console.log(unique(arr));
