5d5ff5f2c083c1fb26f09a7c23e927b3f5078f9e
minghua233 <1540684370@qq.com>
2020-12-19 16:35:33 +0800

    数组扁平

---
+++ js/手写系列/数组扁平/index.js
@@ -0,0 +1,35 @@
+let arr = [1, [2, [3, 4]]]
+// 递归实现
+// function faltten(arr) {
+//   let res = []
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!Array.isArray(arr[i])) {
+//       res.push(arr[i])
+//     } else {
+//       res = res.concat(faltten(arr[i]))
+//     }
+//   }
+//   return res
+// }
+
+
+// reduce
+// function faltten(arr) {
+//   return arr.reduce(function (prev, next) {
+//     return prev.concat(Array.isArray(next) ? faltten(next) : next)
+//   }, [])
+// }
+
+// 解构
+// [1, [2, [3, 4]]] -> 1, [2, [3, 4]] 先融化一层再取值
+// console.log([].concat(...arr));
+function faltten(arr) {
+  while (arr.some(item => Array.isArray(item))) {
+    arr = [].concat(...arr)
+  }
+  return arr
+}
+
+
+// console.log(faltten(arr));
+
