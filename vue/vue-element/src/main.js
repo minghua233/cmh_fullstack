35327c92a690f089a665a6787dd21402afe0fbc2
minghua233 <1540684370@qq.com>
2020-12-22 18:09:50 +0800

    npm 发布验证码组件

--- vue/vue-element/src/main.js
+++ vue/vue-element/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import verify from 'mh-verify-code'
 
 
 Vue.config.productionTip = false
+Vue.use(verify)
 
 new Vue({
   router,
