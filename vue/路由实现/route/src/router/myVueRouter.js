92f166e99cf8feeee6e909db0107c46f881dda15
minghua233 <1540684370@qq.com>
2020-12-23 14:56:02 +0800

    vue路由实现

--- vue/路由实现/route/src/router/myVueRouter.js
+++ vue/路由实现/route/src/router/myVueRouter.js
@@ -1,8 +1,44 @@
+let Vue = null
 class VueRouter {
-
+  constructor(options) {
+    this.mode = options.mode || 'hash'
+    this.routes = options.routes || []
+    this.routesMap = this.createMap(this.routes)
+  }
+  createMap(routes) {
+    return routes.reduce((pre, current) => {
+      pre[current.path] = current.component
+      return pre
+    }, {})
+  }
 }
-VueRouter.install = function () {
-
+VueRouter.install = function (v) {
+  Vue = v
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options && this.$options.router) { // 如果是根组件
+        this._root = this
+        this._router = this.$options.router
+      } else { // 如果是子组件
+        this._root = this.$parent && this.$parent._root
+      }
+      Object.defineProperty(this, '$router', {
+        get() {
+          return this._root._router
+        }
+      })
+    },
+  })
+  Vue.component('router-link', {
+    render(h) {
+      return h('a', {}, '首页')
+    }
+  })
+  Vue.component('router-view', {
+    render(h) {
+      return h('h1', {}, '首页视图')
+    }
+  })
 }
 
 export default VueRouter
