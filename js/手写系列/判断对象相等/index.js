30f1266ffd90867af97f89e4131c11ffaac01876
minghua233 <1540684370@qq.com>
2020-12-23 17:16:26 +0800

    js 判断对象相等

--- js/手写系列/判断对象相等/index.js
+++ js/手写系列/判断对象相等/index.js
@@ -1,7 +1,25 @@
 function eq(a, b) {
+  // +0, -0
+  if (a === b) return a !== 0 || 1 / a === 1 / b
+  // NaN
+  if (a !== a) return b !== b
+  // null
+  if (a == null || b == null) return false
+  
+  let type = typeof (a)
+  if (type !== 'function' && type !== 'object' && typeof (b) !== 'object') return false
+
+  // 复杂的对象
+  return deepEq(a, b)
 
 }
 
 let a = [1]
 let b = [2]
-console.log(eq(a, b));
+console.log(eq(0, 0)); // true
+console.log(eq(0, -0));
+
+
+// +0 === -0
+// (+0).toString() // '0'
+// (-0).toString() // '0'
