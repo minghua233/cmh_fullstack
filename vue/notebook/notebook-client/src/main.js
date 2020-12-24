d1d436ff983d28f5b97bab7d85fae377c66ec3c8
minghua233 <1540684370@qq.com>
2020-12-24 17:18:13 +0800

    notebook

---
+++ vue/notebook/notebook-client/src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import util from './assets/js/util'
+
+import 'amfe-flexible'
+import { Swipe, SwipeItem, Toast, Icon, Field, Uploader, ActionSheet  } from 'vant';
+import 'vant/lib/index.css';
+
+
+
+Vue.use(Swipe).use(SwipeItem).use(Toast).use(axios).use(Icon).use(Field).use(Uploader).use(ActionSheet );
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.prototype.$util = util
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ vue/notebook/notebook-client/src/router/index.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import StarLogin from '@/views/login/StarLogin'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/starLogin',
+    name: 'starLogin',
+    component: StarLogin,
+    meta: {
+      title: '登录'
+    }
+  },
+  {
+    path: '/starRegister',
+    name: 'starRegister',
+    component: () => import('@/views/register/index'),
+    meta: {
+      title: '注册'
+    }
+  },
+  {
+    path: '/noteClass',
+    name: 'noteClass',
+    component: () => import('@/views/noteClass/index'),
+    meta: {
+      title: '笔记分类'
+    }
+  },
+  {
+    path: '/noteList',
+    name: 'noteList',
+    component: () => import('@/views/noteList/index'),
+    meta: {
+      title: '笔记列表'
+    }
+  },
+  {
+    path: '/noteDetail',
+    name: 'noteDetail',
+    component: () => import('@/views/noteDetail/index'),
+    meta: {
+      title: '笔记详情'
+    }
+  },
+  {
+    path: '/noteEdit',
+    name: 'noteEdit',
+    component: () => import('@/views/noteEdit/index'),
+    meta: {
+      title: '编辑笔记'
+    }
+  }
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
