bc34d8a2e8f2be280ca87b627866f9f8473531a6
minghua233 <1540684370@qq.com>
2021-01-04 15:41:15 +0800

    vue3-demo

---
+++ vue/vue3/vue3-demo/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <h1>{{state.count}} * 2 = {{double}}</h1>
+  <h2>{{num}}</h2>
+  <button @click="add">累加</button>
+</template>
+
+<script>
+// composition api
+import { reactive, computed, ref, onMounted } from 'vue'
+export default {
+  setup() { // 入口
+    // const state = reactive({
+    //   count: 1
+    // })
+    const { state, double } = useCount(1)
+    let num = ref(2)
+
+    function add() {
+      state.count++
+      num.value += 10
+    }
+
+    // const double = computed(() => state.count * 2)
+
+    onMounted(() => {
+      console.log('onMounted');
+    })
+
+    return { state, add, double, num }
+  }
+}
+
+function useCount(count) {
+  const state = reactive({
+    count
+  })
+  const double = computed(() => state.count * 2)
+  return {
+    state, double
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
