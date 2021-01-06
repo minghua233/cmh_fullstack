9914f2583226c9c20cd31e3d331be60ab75f3985
minghua233 <1540684370@qq.com>
2021-01-06 16:52:30 +0800

    vue3 reactive

--- vue/vue3/vue-next/reactive.js
+++ vue/vue3/vue-next/reactive.js
@@ -1,9 +1,12 @@
 // vue3.0 响应式原理
 // 2.0 默认递归，数组改变length属性不会触发视图更新，对象不存在的属性无法被拦截
+
+let toProxy = new WeakMap() // 原对象：代理过的对象
+let toRaw = new WeakSet() // 代理过的对象：原对象
+
 function isObject(val) {
   return typeof val === 'object' && val !== null
 }
-
 
 function reactive(target) {
   // 创建响应式对象
@@ -14,6 +17,12 @@ function createReactiveObject(target) {
   if (!isObject(target)) { // 如果不是对象，就直接返回
     return target
   }
+
+  let proxy = toProxy.get(target)
+  if (proxy) {
+    return proxy
+  }
+
   let baseHandler = {
     get(target, key, receiver) {
       console.log('获取');
@@ -39,19 +48,25 @@ function createReactiveObject(target) {
     }
   }
   let observed = new Proxy(target, baseHandler)
+  toProxy.set(target, observed)
+  toRaw.add(observed, target)
   return observed
 }
 
-let proxy = reactive({
-  name: 'wn',
-  age: {
-    n: 18
-  }
-})
+// let proxy = reactive({
+//   name: 'wn',
+//   age: {
+//     n: 18
+//   }
+// })
 // proxy.age
 // proxy.name
 // proxy.name = 'cz'
 // delete proxy.name
 // console.log(proxy.name);
-proxy.sex = 'male'
-console.log(proxy.sex);
+// proxy.sex = 'male'
+// console.log(proxy.sex);
+
+let proxy = reactive([1, 2, 3])
+proxy.push(4)
+console.log(proxy);
