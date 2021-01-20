a1f08dba2944c5c7957260c6f700792e0dfb125a
minghua233 <1540684370@qq.com>
2021-01-20 13:58:42 +0800

    node express

--- node/node-learn/game.js
+++ node/node-learn/game.js
@@ -1,25 +1,31 @@
 // 石头剪刀布
 // console.log(process.argv);
 
-var playerAction = process.argv[process.argv.length - 1]
+// var playerAction = process.argv[process.argv.length - 1]
 // console.log(playerAction);
 
-// 电脑随机生成三个
-var random = Math.random() * 3
-if (random < 1) {
-  var computerAction = 'rock'
-} else if (random > 2) {
-  var computerAction = 'scissor'
-} else {
-  var computerAction = 'paper'
+module.exports = function (playerAction) {
+  // 电脑随机生成三个
+  var random = Math.random() * 3
+  if (random < 1) {
+    var computerAction = 'rock'
+  } else if (random > 2) {
+    var computerAction = 'scissor'
+  } else {
+    var computerAction = 'paper'
+  }
+
+  // 进行比较
+  console.log(`电脑出：${computerAction}`);
+  if (computerAction === playerAction) {
+    console.log('平局');
+    return 0
+  } else if (computerAction === 'rock' && playerAction === 'paper' || computerAction === 'scissor' && playerAction === 'rock' || computerAction === 'paper' && playerAction === 'scissor') {
+    console.log('你赢了');
+    return 1
+  } else {
+    console.log('你输了');
+    return 2
+  }
 }
 
-// 进行比较
-console.log(`电脑出：${computerAction}`);
-if (computerAction === playerAction) {
-  console.log('平局');
-} else if (computerAction === 'rock' && playerAction === 'paper' || computerAction === 'scissor' && playerAction === 'rock' || computerAction === 'paper' && playerAction === 'scissor') {
-  console.log('你赢了');
-} else {
-  console.log('你输了');
-}
