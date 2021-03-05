00f5ccae4ba9be78bfdbba483dc9bcb54365d8be
minghua233 <1540684370@qq.com>
2021-03-05 10:40:36 +0800

    动态规划

--- datastruct/数据结构/动态规划/index.js
+++ datastruct/数据结构/动态规划/index.js
@@ -1,13 +1,28 @@
 //爬楼梯
 
 // 记忆化搜索（优化后的递归） 自顶向下
-const f = []
+// const f = []
+// const climbStairs = function (n) {
+//   if (n === 1) return 1
+//   if (n === 2) return 2
+//   // 递归计算
+//   if (f[n] === undefined) f[n] = climbStairs(n - 1) + climbStairs(n - 2)
+//   return f[n]
+// }
+
+// 动态规划 自顶向上
+// f(n) = f(n - 1) + f(n - 2)
+
 const climbStairs = function (n) {
-  if (n === 1) return 1
-  if (n === 2) return 2
-  // 递归计算
-  if (f[n] === undefined) f[n] = climbStairs(n - 1) + climbStairs(n - 2)
+  const f = []
+  // 初始化已知值
+  f[1] = 1
+  f[2] = 2
+  for (let i = 3; i <= n; i++) {
+    f[i] = f[i - 2] + f[i - 1]
+  }
   return f[n]
 }
 
-// 动态规划 自顶向上
+// 最优子结构
+// 重叠子问题
--- datastruct/数据结构/动态规划/index2.js
+++ datastruct/数据结构/动态规划/index2.js
@@ -1,7 +1,10 @@
 //给定不同面额的硬币和一个总金额，编写一个函数来计算可以凑成总金额所需的最小硬币个数，
 // 如果没有任何一种硬币组合能完成，返回-1
 
-coins = [1, 2 ,5], amount = 11
+// f(x) = Math.min(f(36 - c1) + 1, f(36 - c2) + 1, f(36 - c3) + 1, ..., f(36 - cn) + 1)
+// f(0) = 0
+
+// coins = [1, 2, 5], amount = 11 
 const coinChange = function (coins, amount) {
   // 用于保存每个目标总额对应的最小硬币个数
   const f = [];
@@ -13,7 +16,10 @@ const coinChange = function (coins, amount) {
     for (let j = 0; j < coins.length; j++) {
       // 目标总额大于硬币面额
       if (i - coins[j] >= 0) {
+        console.log('总面额' + i);
+        console.log('硬币面额' + coins[j]);    
         f[i] = Math.min(f[i], f[i - coins[j]] + 1);
+        console.log('最少硬币数量'+f[i]);
       }
     }
   }
@@ -24,3 +30,5 @@ const coinChange = function (coins, amount) {
 
   return f[amount];
 };
+
+console.log(coinChange([1, 2, 5], 11));
