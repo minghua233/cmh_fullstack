a6296dda1f25983720ad31792cb2edd3f08e49f7
minghua233 <1540684370@qq.com>
2021-03-13 21:03:31 +0800

    interview

---
+++ interview/js面试/once.js
@@ -0,0 +1,16 @@
+
+function test() { console.log('test') }
+
+var once = function (fn) {
+  var isFirst = true;
+  return function () {
+    if (isFirst) {
+      isFirst = !isFirst;
+      fn();
+    }
+  };
+};
+
+var b = once(test);
+b(); // 'test'
+b()
