b3de883f050ae57347fc67191c2746a5b3faeb56
minghua233 <1540684370@qq.com>
2021-03-25 17:58:33 +0800

    interview

--- interview/my/test.js
+++ interview/my/test.js
@@ -1,30 +1,11 @@
-// let name = 'cms'
-// let obj = {
-//   name: 'hh',
-//   fn() {
-//     console.log(this.name, 'fn');
-//   },
-//   fn1: () => {
-//     console.log(this.name, 'fn1');
-//   }
-// }
-// let fn3 = obj.fn1
-// console.log(obj.fn());
-// console.log(obj.fn1());
-// console.log(fn3());
+class ClassName {
+  static name = 'static variable'
+  name = 'abc'
+  constructor() {
+    this.fn = function () { }
+  }
+  fn() { 
 
-let name = 'cms'
-let obj = {
-  name: 'hh',
-  fn() {
-    console.log(this.name, 'fn');
-  },
-  fn1: () => {
-    console.log(this.name, 'fn1');
   }
 }
-console.log(window.name);
-let fn3 = obj.fn1
-obj.fn()
-obj.fn1()
-fn3()
+console.log(ClassName.name);
